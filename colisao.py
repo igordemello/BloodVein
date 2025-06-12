e9bceec865e937ca4cc6300ef0dc908b85accd91
Igor Mello <igor.oliveira.mello@gmail.com>
2025-06-12 10:27:52 -0300

    Criada classe Colisão

---
+++ colisao.py
@@ -0,0 +1,27 @@
+from pygame import *
+import sys
+from pygame.locals import QUIT
+import math
+
+class Colisao:
+    def __init__(self, mapa, player, inimigos):
+        self.mapa = mapa
+        self.player = player
+        self.inimigos = inimigos
+
+    def checar_colisoes(self):
+        self._colisao_player_mapa()
+        self._colisao_player_inimigos()
+
+    #underline na frente = função privada
+    def _colisao_player_mapa(self):
+        for collider in self.mapa.get_colliders():
+            if self.player.get_hitbox().colliderect(collider):
+                self.player.voltar_posicao()
+
+    def _colisao_player_inimigos(self):
+        for inimigo in self.inimigos:
+            if self.player.get_hitbox().colliderect(inimigo.get_hitbox()):
+                self.player.voltar_posicao()
+            if inimigo.get_hitbox().colliderect(self.player.get_hitbox()):
+                inimigo.voltar_posicao()
--- main.py
+++ main.py
@@ -7,14 +7,11 @@ from inimigo import Inimigo
 from player import Player
 from botao import Botao
 from mapa import Mapa
-
+from colisao import Colisao
 
 
 clock = time.Clock()
 SCREEN = display.set_mode((1920, 1080))
-
-
-
 
 
 #Instâncias das classes que foram criadas:
@@ -22,6 +19,7 @@ inimigoQuad = Inimigo(300,700,64,64)
 player = Player(950,600,64,96)
 hud = Hud(player)
 mapa = Mapa("mapa/mapa.txt")
+colisao = Colisao(mapa, player, [inimigoQuad])
 
 while True:
     for ev in event.get():
@@ -46,20 +44,8 @@ while True:
     inimigoQuad.desenhar(SCREEN,(player.x,player.y))
     inimigoQuad.atualizar((player.x,player.y))
 
+    colisao.checar_colisoes()
     
-
-    #colisão do inimigo
-    if player.get_hitbox().colliderect(inimigoQuad.get_hitbox()):
-        player.voltar_posicao()
-    if inimigoQuad.get_hitbox().colliderect(player.get_hitbox()):
-        inimigoQuad.voltar_posicao()
-    
-        
-    #colisão com o mapa:
-    for collider in mapa.get_colliders():
-        if player.get_hitbox().colliderect(collider):
-            player.voltar_posicao()
-
     #algo estranho: de vez em quando ele entende como ataque mesmo nao estando exatamente na hitbox
     if player.atacou:
         _, hitbox_espada = player.get_rotated_rect_ataque(mouse_pos)
@@ -69,9 +55,5 @@ while True:
             player.hp += 10
             if player.hp > 100:
                 player.hp = 100
-
-            
-    
-
 
     display.update()
