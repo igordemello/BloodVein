59a1047bc7ea544497b61082007816f7c33b9de8
Igor Mello <igor.oliveira.mello@gmail.com>
2025-06-14 00:48:35 -0300

    implementada colisão entre o inimigo e o mapa, porém está comentada até evoluir a IA do inimigo

--- colisao.py
+++ colisao.py
@@ -12,6 +12,7 @@ class Colisao:
     def checar_colisoes(self):
         self._colisao_player_mapa()
         self._colisao_player_inimigos()
+        #self._colisao_inimgos_mapa()
 
     #underline na frente = função privada
     def _colisao_player_mapa(self):
@@ -30,3 +31,11 @@ class Colisao:
                 self.player.voltar_posicao()
             if inimigo.get_hitbox().colliderect(self.player.get_hitbox()):
                 inimigo.voltar_posicao()
+
+    # colisão entre o inimigo e o mapa, porém atualmente a IA de seguir o personagem não prevê esse tipo de coisa,
+    # então o inimigo fica agarrado. Descomentar no checar_colisoes quando descomentar aqui
+    # def _colisao_inimgos_mapa(self):
+    #     for collider in self.mapa.get_colliders():
+    #         for inimigo in self.inimigos:
+    #             if inimigo.get_hitbox().colliderect(collider):
+    #                 inimigo.voltar_posicao()
