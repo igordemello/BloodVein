6fc35f95f219723c7d3b17809a5d961e2d49f654
Igor Mello <igor.oliveira.mello@gmail.com>
2025-07-15 20:16:05 -0300

    hud nova em processo (falta a stamina e mana bar serem dinamicas)

--- hud.py
+++ hud.py
@@ -11,33 +11,26 @@ from screen_shake import screen_shaker
 class Hud:
     def __init__(self, player,tela):
         self.player = player
-        self.bracoHp_fundo = transform.scale(image.load('assets/UI/HDEmptyHealthUI - Rotacionada.png').convert_alpha(),
-                                             (192, 576))
-        self.bracoHp_cima = transform.scale(image.load('assets/UI/HDFillHealthUI - Rotacionada.png').convert_alpha(),
-                                            (192, 576))
-        self.bracoSt_fundo = transform.flip(
-            transform.scale(image.load('assets/UI/HDEmptyManaUI - Rotacionada.png').convert_alpha(), (192, 576)), True,
-            False)
-        self.bracoSt_cima = transform.flip(
-            transform.scale(image.load('assets/UI/HDFillManaUI - Rotacionada.png').convert_alpha(), (192, 576)), True,
-            False)
-
-        self.almaIcon = transform.scale(image.load('assets/Itens/alma.png').convert_alpha(), (96, 96))
-        self.hud = image.load('assets/UI/Hud.png').convert_alpha()
+        
         self.fundo = image.load('assets/UI/tela_fundo1.png').convert_alpha()
-        self.fundo2 = image.load('assets/UI/tela_fundo2.jpeg').convert_alpha()
-
-        self.statsArma = transform.scale(image.load('assets/UI/stats.png').convert_alpha(),(300*1.3,250*1.3))
-
-        self.chanceCriticoTita= transform.scale(image.load('assets/UI/espadatitaCrit.png').convert_alpha(), (100,80))
-
-        self.armaIcon = transform.scale(image.load(self.player.arma.spriteIcon).convert_alpha(), (96,96))
-
         self.fundo = transform.scale(self.fundo, (1920, 1080))
 
+        self.hud = image.load('assets/UI/HUD.png').convert_alpha()
+        self.hud = transform.scale(self.hud, (self.hud.get_width()*0.9, self.hud.get_height()*0.9))
+
+        self.full_stamina_mana = image.load('assets/UI/full_stamina_mana.png').convert_alpha()
+        self.full_stamina_mana = transform.scale(self.full_stamina_mana, (self.full_stamina_mana.get_width()*0.9, self.full_stamina_mana.get_height()*0.9))
+
+        self.full_hp = image.load('assets/UI/full_hp.png').convert_alpha()
+        self.full_hp = transform.scale(self.full_hp, (self.full_hp.get_width()*0.9, self.full_hp.get_height()*0.9))
+
+        self.almaIcon = transform.scale(image.load('assets/Itens/alma.png').convert_alpha(), (76,76))
+        
+        self.statsArma = transform.scale(image.load('assets/UI/stats.png').convert_alpha(),(300*1.3,250*1.3))
+        
         self.stats_alpha = None
 
-        # Variáveis para o efeito de shake
+
         self.combo_shake_intensity = 0
         self.combo_shake_duration = 0
         self.last_combo_count = 0
@@ -52,9 +45,6 @@ class Hud:
 
     def desenhaFundo(self):
         self.tela.blit(self.fundo, (0, 0))
-
-    def desenhaFundo2(self):
-        self.tela.blit(self.fundo2, (0,0))
 
     def update(self, dt):
         if self.combo_shake_duration > 0:
@@ -83,7 +73,7 @@ class Hud:
 
     def desenhar(self, minimal=False):
         offset_x, offset_y = screen_shaker.offset
-        almas_font = font.Font('assets/Fontes/alagard.ttf', 48)
+        almas_font = font.Font('assets/Fontes/alagard.ttf', 55)
         almas = almas_font.render(f"{self.player.almas}x", True, (243, 236, 215))
 
         total_hits = self.player.hits + self.player.hits_projetil
@@ -99,7 +89,7 @@ class Hud:
 
 
         almas_rect = almas.get_rect()
-        almas_rect.right = 1540
+        almas_rect.right = 1830
         almas_rect.top = 68
 
         base_combo_rect = combo.get_rect()
@@ -114,102 +104,37 @@ class Hud:
         comboMult_rect.centerx = base_combo_rect.centerx + self.shake_offset[0] * 0.7
         comboMult_rect.top = base_combo_rect.bottom - 15 + self.shake_offset[1] * 0.7
 
-        if not minimal:
-            self.tela.blit(self.hud, (0, 0))
+        if minimal:
+            return
+        
+        #braços e coração fundo
+        meio = self.tela.get_width() // 2
+        x_hud = meio - self.hud.get_width() // 2
+        
+        self.tela.blit(self.hud, (x_hud, 885))
 
-        # HP Bar
-        larguraHp_total = self.bracoHp_cima.get_width()
-        alturaHp_total = self.bracoHp_cima.get_height() - 100
+
+        #braços e coracao dinamico
+        alturaHp_total = self.full_hp.get_height() 
         proporcaoHp = max(0, min(1, self.player.hp / 100))
         altura_visivelHp = int(alturaHp_total * proporcaoHp)
 
-        self.tela.blit(self.bracoHp_fundo, (15 + offset_x, 510 + offset_y))
-
         if altura_visivelHp > 0:
-            y_corte = self.bracoHp_cima.get_height() - altura_visivelHp
-            barra_cheia_cortada = self.bracoHp_cima.subsurface((0, y_corte, larguraHp_total, altura_visivelHp)).copy()
-            self.tela.blit(barra_cheia_cortada, (15 + offset_x, 510 + y_corte + offset_y))
+            y_corte = self.full_hp.get_height() - altura_visivelHp
+            barra_cheia_cortada = self.full_hp.subsurface((0, y_corte, self.full_hp.get_width(), altura_visivelHp)).copy()
+            self.tela.blit(barra_cheia_cortada, (x_hud, 885 + y_corte))
+            # draw.rect(self.tela, (255, 0, 0), (x_hud, 885 + y_corte, self.full_hp.get_width(), altura_visivelHp), 2)
 
+        self.tela.blit(self.full_stamina_mana, (x_hud, 885))
+        
 
-        # Stamina Bar
-        braco_stamina = self.bracoSt_fundo
-        larguraSt_total = self.bracoSt_cima.get_width()
-        alturaSt_total = self.bracoSt_cima.get_height() - 70
-        proporcaoSt = max(0, min(1, self.player.mp / 100))
-        altura_visivelSt = int(alturaSt_total * proporcaoSt)
-
-        self.tela.blit(braco_stamina, (1715+ offset_x, 510+offset_y))
-
-        if altura_visivelSt > 0:
-            y_corte = self.bracoSt_cima.get_height() - altura_visivelSt
-            barra_cheia_cortada = self.bracoSt_cima.subsurface((0, y_corte, larguraSt_total, altura_visivelSt)).copy()
-            self.tela.blit(barra_cheia_cortada, (1715, 510 + y_corte))
-
-
-
-        if minimal:
-            return
-
-        #legendas de teclas
-        base_y = self.tela.get_height() - 40
-
-        fonte_tecla = font.Font("assets/fontes/212 Keyboard.otf", 32)
-        fonte_texto = font.Font("assets/fontes/alagard.ttf", 28)
-
-        partes = [
-            ("w", fonte_tecla, (243, 236, 215)),
-            (" dash   ", fonte_texto, (243, 236, 215)),
-            ("I", fonte_tecla, (243, 236, 215)),
-            (" inventário   ", fonte_texto, (243, 236, 215)),
-            ("e", fonte_tecla, (243, 236, 215)),
-            (" mapa   ", fonte_texto, (243, 236, 215)),
-            ("Q", fonte_tecla, (243, 236, 215)),
-            (" item ativo", fonte_texto, (243, 236, 215))
-        ]
-
-        # Calcular largura total do conjunto
-        largura_total = sum(fonte.size(texto)[0] for texto, fonte, _ in partes)
-
-        # Começar no centro da tela, alinhado à esquerda
-        x = (self.tela.get_width() - largura_total) // 2
-
-        # Desenhar tudo alinhado a partir do centro
-        for texto, fonte, cor in partes:
-            renderizado = fonte.render(texto, True, cor)
-            self.tela.blit(renderizado, (x, base_y))
-            x += renderizado.get_width()
-    
-
-        # Desenha itens
-        for i, (item, qtd) in enumerate(self.player.itens.items()):
-            x = 200 + (i % 6) * 75
-            y = 25 + (i // 6) * 64
-            sprite = transform.scale(item.sprite, (64, 64))
-            self.tela.blit(sprite, (x, y))
-
-        # Itens ativos
-        if self.player.itemAtivo is not None:
-            for x in range(0, self.player.itemAtivo.usos):
-                draw.rect(self.tela, (0, 255, 0), (50 + (x * 25), 144, 20, 8))
-            sprite = transform.scale(self.player.itemAtivo.sprite, (96, 96))
-            self.tela.blit(sprite, (60, 40))
+        
 
         # Almas
-        self.tela.blit(self.almaIcon, (1545, 33))
+        self.tela.blit(self.almaIcon, (almas_rect.right, 43))
         self.tela.blit(almas, almas_rect)
 
-        self.tela.blit(self.armaIcon, (1710, 48))
 
-        #Se espada do titã, mostra chance critico:
-        if isinstance(self.player.arma, EspadaDoTita):
-            crit_font = font.Font('assets/Fontes/alagard.ttf', 36)
-            critico = crit_font.render(f"{self.player.arma.chanceCritico}%", True, (243, 236, 215))
-            crit_rect = critico.get_rect()
-            crit_rect.right = 1795
-            crit_rect.top = 170
-
-            self.tela.blit(self.chanceCriticoTita, (1710,150,110,74))
-            self.tela.blit(critico, (crit_rect))
 
         if total_hits > 0:
             self.tela.blit(combo, combo_rect.topleft)
