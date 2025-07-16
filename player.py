8e31b83c5052f3e21be6ef3d998dff235348a9df
aabobrinha <nicholasestevesf@gmail.com>
2025-07-16 03:26:42 -0300

    Poções finalizadas

--- player.py
+++ player.py
@@ -72,8 +72,8 @@ class Player():
         self.hp = hp
         self.hpMax = hp
 
-        self.pocoesHp = 4
-        self.pocoesMp = 4
+        self.pocoesHp = 0
+        self.pocoesMp = 0
 
 
         self.mpModificador = 1
